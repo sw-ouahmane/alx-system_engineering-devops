35bfe2b611b7e2aac8b54dc979520ddf72de6e2c
Abdallah <sw.ouahmane@gmail.com>
2024-06-05 13:10:19 +0100

    check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
-
+"""Write a recursive function that queries the Reddit API, parses the
+title of all hot articles, and prints a sorted count of given keywords"""
 from requests import get
 
 
-def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+def count_words(subreddit, word_list, after="", word_dict={}):
+    """Returns the top ten hot words for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    response = get(url, headers={"User-Agent": "My-User-Agent"},
+                   allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        for child in data.get("children"):
+            for word in [txt.lower() for txt
+                         in child.get("data").get("title").split()]:
+                if word in word_list:
+                    if word in word_dict:
+                        word_dict[word] += 1
+                    else:
+                        word_dict[word] = 1
+        after = data.get("after")
+        if after:
+            count_words(subreddit, word_list, after, word_dict)
+            return
+        for word in word_dict:
+            print(f"{word}: {word_dict[word]}")
