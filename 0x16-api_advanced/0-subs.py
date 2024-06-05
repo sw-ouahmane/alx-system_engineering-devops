d125486e257f284accc8bf08836d9f6415852388
Abdallah <sw.ouahmane@gmail.com>
2024-06-05 13:01:05 +0100

    Add

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
-"""Returns the number of subscribers for a given subreddit"""
+"""Module to return the number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
+    """Returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        print(number_of_subscribers(sys.argv[1]))
+    else:
+        print("Please pass an argument for the subreddit to search.")
